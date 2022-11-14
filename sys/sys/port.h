b44bd32fb5a45e8d5063c7497133d27965bd54ba
Stephan Wib <stephanwib@googlemail.com>
2022-11-14 09:07:07 +0100

    Add port.h and some Haiku examples

---
+++ sys/sys/port.h
@@ -0,0 +1,115 @@
+/*      $NetBSD: port.h,v 1.00 2022/11/08 16:57:28 stephanwib Exp $        */
+
+/*-
+ * Copyright (c) 2015 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Stephan Wiebusch.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef _SYS_PORT_H
+#define _SYS_PORT_H
+
+#define PORT_MAX_NAME_LENGTH 32
+
+
+typedef int64_t bigtime_t;
+typedef int32_t port_id;
+
+
+typedef struct port_info {
+	port_id     port;
+	pid_t       pid;
+	char	    	name[PORT_MAX_NAME_LENGTH];
+	int32_t		capacity;		
+	int32_t		queue_count;	/* messages in queue */
+	int32_t		total_count;	/* total messages read */
+} port_info;
+
+
+
+port_id		create_port(int32_t, const char *);
+port_id		find_port(const char *);
+ssize_t		read_port(port_id, int32_t *, void *,size_t);
+ssize_t		read_port_etc(port_id, int32_t *, void *,	size_t, uint32_t, bigtime_t);
+int     	write_port(port_id, int32_t, const void *, size_t);
+int     	write_port_etc(port_id, int32_t, const void *, size_t, uint32_t, bigtime_t);
+int     	close_port(port_id);
+int     	delete_port(port_id);
+ssize_t		port_buffer_size(port_id);
+ssize_t		port_buffer_size_etc(port_id, uint32_t,	bigtime_t);
+ssize_t		port_count(port_id);
+int     	set_port_owner(port_id, pid_t);
+
+
+#ifdef _KERNEL
+
+#include <sys/queue.h>
+#include <sys/mutex.h>
+#include <sys/condvar.h>
+
+enum flags {
+    B_TIMEOUT               = 0x8,
+    B_RELATIVE_TIMEOUT      = 0x8,
+    B_ABSOLUTE_TIMEOUT      = 0x10
+};
+
+struct kport {
+  LIST_ENTRY(kport)       kp_entry;        /* global list entry */
+  SIMPLEQ_HEAD(, kp_msg)  kp_msgq;        /* head of message queue */
+  kmutex_t                kp_interlock;   /* lock on this kport */
+  kcondvar_t              kp_rdcv;        /* reader CV */
+  kcondvar_t              kp_wrcv;        /* writer CV */
+  port_id                 kp_id;          /* id of this port */
+  pid_t                   kp_owner;       /* owner PID assigned to this port */
+  char                    *kp_name;       /* name of this port */
+  size_t                  kp_namelen;     /* length of name */
+  int                     kp_state;       /* state of this port */
+  int                     kp_nmsg;        /* number of messages in this port */
+  int                     kp_total_count; /* number of messages already read */  
+  int                     kp_qlen;        /* queue length */
+  int                     kp_waiters;     /* count of waiters */
+  uid_t                   kp_uid;         /* creator uid */
+  gid_t                   kp_gid;         /* creator gid */
+};
+
+struct kp_msg {
+  SIMPLEQ_ENTRY(kp_msg)   kp_msg_next;          /* message queue entry */
+  int32_t                 kp_msg_code;          /* message code */
+  size_t                  kp_msg_size;          /* bytes in message */
+  uid_t                   kp_msg_sender_uid;    /* uid of sender */
+  gid_t                   kp_msg_sender_gid;    /* gid of sender */
+  pid_t                   kp_msg_sender_pid;    /* pid of sender */
+  char                    *kp_msg_buffer;       /* message data */
+};
+
+
+/* Prototypes */
+void kport_init(void);
+
+#endif	/* _KERNEL */
+
+#endif	/* _SYS_PORT_H_ */
