e608794b3ae2dd2f8f05c906650890bb65c6e4ab
stephanwib <stephanwib@googlemail.com>
2025-01-09 15:33:46 +0100

    Update time.c

--- lib/libroot/time.c
+++ lib/libroot/time.c
@@ -1,6 +1,6 @@
 
 
-
+#include <OS.h>
 #include <sys/sysctl.h>
 #include <sys/time.h>
 #include <stdint.h>
@@ -27,11 +27,11 @@ bigtime_t system_time() {
 }
 
 
-time_t real_time_clock() {
+uint32 real_time_clock() {
     struct timeval tv;
 
     if (gettimeofday(&tv, NULL) != 0) {
-        return (time_t)-1;
+        return -1;
     }
 
     return tv.tv_sec;
