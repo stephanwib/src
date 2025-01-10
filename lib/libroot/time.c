5707a5ebb53f38c66b4dc027dab53cda2d8e03a6
stephanwib <stephanwib@googlemail.com>
2025-01-10 14:27:59 +0100

    Update time.c

--- lib/libroot/time.c
+++ lib/libroot/time.c
@@ -11,14 +11,13 @@ bigtime_t system_time() {
     struct timeval boottime;
     size_t size = sizeof(boottime);
     struct timeval now;
+    struct timezone timezone;
     
     if (sysctlbyname("kern.boottime", &boottime, &size, NULL, 0) != 0) {
         return -1;
     }
 
-    if (gettimeofday(&now, NULL) != 0) {
-        return -1;
-    }
+    (void)gettimeofday(&now, &timezone);
 
     // Calculate the uptime in microseconds
     time_t seconds = now.tv_sec - boottime.tv_sec;
@@ -28,22 +27,20 @@ bigtime_t system_time() {
 
 
 uint32 real_time_clock() {
-    struct timeval tv;
-
-    if (gettimeofday(&tv, NULL) != 0) {
-        return -1;
-    }
-
+    struct timeval now;
+    struct timezone timezone;
+    
+    (void)gettimeofday(&now, &timezone);
+        
     return tv.tv_sec;
 }
 
 // Get the current real-time clock in microseconds
 bigtime_t real_time_clock_usecs() {
-    struct timeval tv;
-
-    if (gettimeofday(&tv, NULL) != 0) {
-        return -1;
-    }
+    struct timeval now;
+    struct timezone timezone;
+    
+    (void)gettimeofday(&now, &timezone);
 
     return (bigtime_t)tv.tv_sec * 1000000 + tv.tv_usec;
 }
@@ -51,14 +48,13 @@ bigtime_t real_time_clock_usecs() {
 // Set the real-time clock to a new value
 void set_real_time_clock(int32 new_time) {
     struct timeval tv;
-
+    struct timezone timezone;
+    
     tv.tv_sec = new_time;
     tv.tv_usec = 0;
 
-    // Use settimeofday to update the system clock
-    if (settimeofday(&tv, NULL) != 0) {
-        return;
-    }
-
-    return;
+    // XXX: Call to settimeofday() may fail due to several reasons, but this void function
+    // does not allow to report back errors
+    
+    (void)settimeofday(&tv, &timezone);
 }
