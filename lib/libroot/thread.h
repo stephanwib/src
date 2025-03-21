c285b63157fb82424cc3e48b4bd62d04711d706d
stephanwib <stephanwib@googlemail.com>
2025-03-21 13:55:23 +0100

    Update thread.h

--- lib/libroot/thread.h
+++ lib/libroot/thread.h
@@ -21,7 +21,7 @@ typedef struct thread_message {
     char                        tm_buffer[MSG_PRIVATE_BUFFER_SIZE];     /* small message private buffer  */
     size_t                      tm_size;                                /* private data bytes */
     const void                  *tm_external_buffer;                    /* large message external buffer */
-    pthread_cond_t              *tm_msg_cv;                             /* wait for message event */
+    pthread_cond_t              tm_msg_cv;                             /* wait for message event */
 } thread_message;
 
 typedef struct haiku_thread {
