9e9efd8b57cb9dcd7648f53426b43096282e5708
stephanwib <stephanwib@googlemail.com>
2025-05-09 11:23:09 +0200

    Update debug.c

--- lib/libroot/debug.c
+++ lib/libroot/debug.c
@@ -1,1 +1,15 @@
 
+void
+debugger(const char *message)
+{
+    printf("Debugger called: %s\n", message);
+}
+
+
+void debug_printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
