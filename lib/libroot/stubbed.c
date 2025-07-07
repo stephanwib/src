5d4bf211143c1773a79fdb62c85506266c084459
stephanwib <stephanwib@googlemail.com>
2025-07-07 13:15:16 +0200

    Create stubbed.c

---
+++ lib/libroot/stubbed.c
@@ -0,0 +1,11 @@
+
+/* driver API */
+void delete_driver_settings() {}
+void get_driver_settings() {}
+void get_driver_settings_string() {}
+void load_driver_settings() {}
+void load_driver_settings_file() {}
+
+/* ?? */
+void convert_from_stat_beos() {}
+void convert_to_stat_beos() {}
