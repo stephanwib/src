24d1e6423d4d73e4058a4f6453cf39b66a2447ef
stephanwib <stephanwib@googlemail.com>
2025-07-21 10:06:47 +0200

    Update stubbed.c

--- lib/libroot/stubbed.c
+++ lib/libroot/stubbed.c
@@ -10,10 +10,13 @@ void load_driver_settings_file(void);
 void convert_from_stat_beos(void);
 void convert_to_stat_beos(void);
 void __swap_double(void);
+void get_architectures(void);
+void get_secondary_architectures(void);
 void find_path_etc(void);
 void find_paths_etc(void);
 void find_path_for_path_etc(void);
 void fs_stat_index(void);
+void fs_create_index(void);
 void find_directory(void);
 void _kern_transfer_area(void);
 void _kern_get_safemode_option(void);
@@ -32,12 +35,15 @@ void load_driver_settings_file(void) {}
 void convert_from_stat_beos(void) {}
 void convert_to_stat_beos(void) {}
 void __swap_double(void) {}
+void get_architectures(void) {}
+void get_secondary_architectures(void) {}
 
 /* filesystem or similar */
 void find_path_etc(void) {}
 void find_paths_etc(void) {}
 void find_path_for_path_etc(void) {}
 void fs_stat_index(void) {}
+void fs_create_index(void) {}
 void find_directory(void) {}
 
 /* kernel */
