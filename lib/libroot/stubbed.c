462817b34ffd2471ece73806daa23cdc5a96f8f4
stephanwib <stephanwib@googlemail.com>
2025-11-10 13:33:34 +0100

    Update stubbed.c

--- lib/libroot/stubbed.c
+++ lib/libroot/stubbed.c
@@ -1,51 +1,32 @@
+#include <stdio.h>
 
-
-void delete_driver_settings(void);
-void get_driver_settings(void);
-void get_driver_settings_string(void);
-void parse_driver_settings_string(void);
-void load_driver_settings(void);
-void unload_driver_settings(void);
-void load_driver_settings_file(void);
-void convert_from_stat_beos(void);
-void convert_to_stat_beos(void);
-void __swap_double(void);
-void get_architectures(void);
-void get_secondary_architectures(void);
-void find_path_etc(void);
-void find_paths_etc(void);
-void find_path_for_path_etc(void);
-void fs_stat_index(void);
-void fs_create_index(void);
-void find_directory(void);
-void _kern_transfer_area(void);
-void _kern_get_safemode_option(void);
-
+#define STUB_WARNING() \
+    fprintf(stderr, "STUB CALLED: %s() in %s:%d\n", __func__, __FILE__, __LINE__)
 
 /* driver API */
-void delete_driver_settings(void) {}
-void get_driver_settings(void) {}
-void get_driver_settings_string(void) {}
-void parse_driver_settings_string(void) {}
-void load_driver_settings(void) {}
-void unload_driver_settings(void) {}
-void load_driver_settings_file(void) {}
+void delete_driver_settings(void)           { STUB_WARNING(); }
+void get_driver_settings(void)              { STUB_WARNING(); }
+void get_driver_settings_string(void)       { STUB_WARNING(); }
+void parse_driver_settings_string(void)     { STUB_WARNING(); }
+void load_driver_settings(void)             { STUB_WARNING(); }
+void unload_driver_settings(void)           { STUB_WARNING(); }
+void load_driver_settings_file(void)        { STUB_WARNING(); }
 
-/* ?? */
-void convert_from_stat_beos(void) {}
-void convert_to_stat_beos(void) {}
-void __swap_double(void) {}
-void get_architectures(void) {}
-void get_secondary_architectures(void) {}
+/* conversion */
+void convert_from_stat_beos(void)           { STUB_WARNING(); }
+void convert_to_stat_beos(void)             { STUB_WARNING(); }
+void __swap_double(void)                    { STUB_WARNING(); }
+void get_architectures(void)                { STUB_WARNING(); }
+void get_secondary_architectures(void)      { STUB_WARNING(); }
 
-/* filesystem or similar */
-void find_path_etc(void) {}
-void find_paths_etc(void) {}
-void find_path_for_path_etc(void) {}
-void fs_stat_index(void) {}
-void fs_create_index(void) {}
-void find_directory(void) {}
+/* filesystem */
+void find_path_etc(void)                    { STUB_WARNING(); }
+void find_paths_etc(void)                   { STUB_WARNING(); }
+void find_path_for_path_etc(void)           { STUB_WARNING(); }
+void fs_stat_index(void)                    { STUB_WARNING(); }
+void fs_create_index(void)                  { STUB_WARNING(); }
+void find_directory(void)                   { STUB_WARNING(); }
 
 /* kernel */
-void _kern_transfer_area(void) {}
-void _kern_get_safemode_option(void) { }
+void _kern_transfer_area(void)              { STUB_WARNING(); }
+void _kern_get_safemode_option(void)        { STUB_WARNING(); }
